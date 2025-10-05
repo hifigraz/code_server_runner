38d41ffd0be87df3d95f87945d7f16b2dbfc7ac0
Robert Ulmer <rulmer@lr.htlweiz.at>
2025-10-05 18:40:23 +0200

    api robustness

--- api/run.sh
+++ api/run.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env sh
 echo running api
+while [ ! -e pyproject.toml ]; do
+  sleep 5
+done
+echo installing package
 pip install .
 chmod -R a+rwX *egg-info
-while [ ! -e IVahit/scripts.py ]; do
-  sleep 5
-done
+echo starting api
 while true; do
   fastapi dev IVahit/scripts.py --host 0.0.0.0 --port 80 --proxy-headers --root-path /api
+  echo restarting api
   sleep 5
 done
+
