195b84749467bafe4699c00c3b54519a4d2017ab
Robert Ulmer <rulmer@lr.htlweiz.at>
2025-10-05 18:53:16 +0200

    api robustness

--- api/run.sh
+++ api/run.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/env sh
-running=true
+
+running=1
+
 stop_server() {
-  running=false
+  running=0
 }
+
 trap SIGTERM stop_server 
 
 echo running api
-while [ ${running} && ! -e pyproject.toml ]; do
+while [ "${running}" -ne "0" && ! -e "pyproject.toml" ]; do
+  echo waiting for project
   sleep 5
 done
-if [ ${running} && -e pyproject.toml ]; then
+
+if [ "${running}" -ne "0" && -e "pyproject.toml" ]; then
   echo installing package
   pip install .
   chmod -R a+rwX *egg-info
 fi
+
 echo starting api
-while ${running}; do
+while [ "${running}" -ne "0" ]; do
   fastapi dev IVahit/scripts.py --host 0.0.0.0 --port 80 --proxy-headers --root-path /api
   echo restarting api
   sleep 5
