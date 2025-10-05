de389671fe75c65acda48760f7f6639f3fd5db9e
Robert Ulmer <rulmer@lr.htlweiz.at>
2025-10-05 18:37:04 +0200

    api robustness

--- api/run.sh
+++ api/run.sh
@@ -2,4 +2,10 @@
 echo running api
 pip install .
 chmod -R a+rwX *egg-info
-fastapi dev IVahit/scripts.py --host 0.0.0.0 --port 80 --proxy-headers --root-path /api
+while [ ! -e IVahit/scripts.py ]; do
+  sleep 5
+done
+while true; do
+  fastapi dev IVahit/scripts.py --host 0.0.0.0 --port 80 --proxy-headers --root-path /api
+  sleep 5
+done
