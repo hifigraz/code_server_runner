f150298cb9ba8288425f8a268eeb014988ed436f
Robert Ulmer <rulmer@lr.htlweiz.at>
2025-11-19 15:31:39 +0100

    good

--- api/run.sh
+++ api/run.sh
@@ -5,11 +5,27 @@ running=1
 stop_server() {
   running=0
 }
+trap stop_server TERM 
 
-trap SIGTERM stop_server 
+getWorkDir() {
+  for i in */; do
+    if [ -d "$i" ]; then
+      cd $i
+      if [ -e "pyproject.toml" ]; then
+        pwd
+        return 0
+      fi
+      cd ..
+    fi
+  done
+  return 1
+}
+
+
 
 echo running api
-while [ "${running}" -ne "0" ] && [ ! -e "pyproject.toml" ]; do
+while [ "${running}" -ne "0" ]; do
+  getWorkDir && break
   echo waiting for project
   sleep 5
 done
@@ -22,7 +38,9 @@ fi
 
 echo starting api
 while [ "${running}" -ne "0" ]; do
-  fastapi dev IVahit/scripts.py --host 0.0.0.0 --port 80 --proxy-headers --root-path /api
+  script_name="$(find -name scripts.py |grep -v ^./build)"
+  echo "Script ${script_name}"
+  fastapi dev "${script_name}" --host 0.0.0.0 --port 80 --proxy-headers --root-path /api
   echo restarting api
   sleep 5
 done
