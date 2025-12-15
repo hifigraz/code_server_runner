403aeb1dccc828d78c49bf798f8504dbe2a4f92d
Robert Ulmer <rulmer@lr.htlweiz.at>
2025-12-15 22:42:03 +0100

    try to harden startup

--- api/run.sh
+++ api/run.sh
@@ -1,6 +1,35 @@
 #!/usr/bin/env sh
 
+
+log() {
+  echo LOG $* >&2
+}
+
+fail() {
+  exit_code=$1
+  shift
+  log $*
+  exit ${exit_code}
+}
+
 running=1
+
+linkFiles() {
+  log linkFiles called with $*
+  cd $1
+  while [ "0" -ne "${running}" ]; do
+    mkdir -p /workspace/
+    for i in $(ls -A /$1 | grep -v egg-info); do
+      if [ -e /workspace/${i} ]; then
+        log skipping file $1/$i
+      else
+        log "linkFile $1/$i -> /workspace/$i"
+        ln -s $1/$i /workspace/$i
+      fi
+    done
+    sleep 5
+  done
+}
 
 stop_server() {
   running=0
@@ -8,39 +37,43 @@ stop_server() {
 trap stop_server TERM 
 
 getWorkDir() {
-  for i in */; do
-    if [ -d "$i" ]; then
-      cd $i
-      if [ -e "pyproject.toml" ]; then
-        pwd
-        return 0
-      fi
-      cd ..
-    fi
+  log getWorkDir looking for pyproject.toml
+  for i in $(find /workspace_ro -name pyproject.toml); do
+    log getWorkDir got pyproject.toml
+    echo $(dirname $i)
+    return 0
   done
   return 1
 }
 
-
-
-echo running api
+log running api
 while [ "${running}" -ne "0" ]; do
-  getWorkDir && break
-  echo waiting for project
+  do_break=0
+  PACKAGE_DIR=$(getWorkDir)
+  if [ 0 -eq $? ]; then
+    linkFiles ${PACKAGE_DIR} &
+    break
+  fi
+  log waiting for project
   sleep 5
 done
+  
+cd /workspace
 
 if [ "${running}" -ne "0" ] && [ -e "pyproject.toml" ]; then
-  echo installing package
+  log installing package
   pip install .
-  chmod -R a+rwX *egg-info
 fi
 
 echo starting api
 while [ "${running}" -ne "0" ]; do
-  script_name="$(find -name scripts.py |grep -v ^./build)"
+  log files $(ls /workspace/)
+  find -L -name scripts.py | grep -v ^./build
+
+  script_name="$(find -L -name scripts.py |grep -v ^./build)"
   echo "Script ${script_name}"
-  fastapi dev "${script_name}" --host 0.0.0.0 --port 80 --proxy-headers --root-path /api
+
+  fastapi dev ${script_name} --host 0.0.0.0 --port 80 --proxy-headers --root-path /api
   echo restarting api
   sleep 5
 done
